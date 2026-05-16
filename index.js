a8cdd35e7942bdd8681a76a44f8a3676ef86d60e
veyndan <veyndan@gmail.com>
2026-05-16 21:37:40 +0200

    Have UI respond to whether location permissions granted

--- index.js
+++ index.js
@@ -1,28 +1,51 @@
 "use strict";
 
-document.querySelector(`button`).addEventListener(`click`, () => {
-	navigator.geolocation.getCurrentPosition(
-		async (position) => {
-			document.querySelector(`section`).innerText = `Getting temperature…`;
-			const url = new URL(`https://api.open-meteo.com/v1/forecast`);
-			url.searchParams.set(`latitude`, position.coords.latitude.toString());
-			url.searchParams.set(`longitude`, position.coords.longitude.toString());
-			url.searchParams.append(`current`, `temperature_2m`);
-			const response = await fetch(url);
-			if (response.ok) {
-				const weatherData = await response.json();
-				document.querySelector(`section`).innerText = `${weatherData.current.temperature_2m}${weatherData.current_units.temperature_2m}`;
-			} else {
-				// TODO
-			}
-		},
-		(error) => {
-			console.log(`Unable to retrieve your location due to ${error.code}: ${error.message}`);
-		},
-		{
-			enableHighAccuracy: true,
-			maximumAge: 30000,
-			timeout: 27000,
-		},
-	);
-});
+navigator.permissions
+	.query({name: `geolocation`})
+	.then((permissionStatus) => {
+		function setGeolocationElement() {
+			navigator.geolocation.getCurrentPosition(
+				async (position) => {
+					document.querySelector(`section`).innerText = `Getting temperature…`;
+					const url = new URL(`https://api.open-meteo.com/v1/forecast`);
+					url.searchParams.set(`latitude`, position.coords.latitude.toString());
+					url.searchParams.set(`longitude`, position.coords.longitude.toString());
+					url.searchParams.append(`current`, `temperature_2m`);
+					const response = await fetch(url);
+					if (response.ok) {
+						const weatherData = await response.json();
+						document.querySelector(`section`).innerText = `${weatherData.current.temperature_2m}${weatherData.current_units.temperature_2m}`;
+					} else {
+						// TODO
+					}
+				},
+				(error) => {
+					console.log(`Unable to retrieve your location due to ${error.code}: ${error.message}`);
+				},
+				{
+					enableHighAccuracy: true,
+					maximumAge: 30000,
+					timeout: 27000,
+				},
+			);
+		}
+
+		switch (permissionStatus.state) {
+			case `prompt`:
+				const buttonElement = document.createElement(`button`);
+				buttonElement.type = `button`;
+				buttonElement.textContent = `Update location permissions`;
+				document.querySelector(`section`).append(buttonElement)
+				buttonElement.addEventListener(`click`, event => {
+					(/** @type {HTMLButtonElement} */ (event.currentTarget)).remove();
+					setGeolocationElement();
+				});
+				break;
+			case `granted`:
+				setGeolocationElement();
+				break;
+			case `denied`:
+				document.querySelector(`section`).innerText = `Location permission denied.`
+				break;
+		}
+	});
